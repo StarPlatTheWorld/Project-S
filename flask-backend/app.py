419b3f870b2f12c1041a0cef087032f38f73607b
Ethan <mcfarlane-e1@ulster.ac.uk>
2024-03-16 18:50:21 +0000

    Installed PyMongo and connected MongoDB to backend for database queries. Added vulnerability route to backend for later version.

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
+
+client = MongoClient("localhost", 27017)
+db = client["ProjectS"]
+
+package_collection = db["packages"]
 
 vulnerabilities = [
     {
@@ -13,8 +20,12 @@ vulnerabilities = [
     }
 ]
 
-@app.route("/api/")
+@app.route("/")
 def index():
+    return jsonify(vulnerabilities)
+
+@app.route("/vulnerabilities")
+def vulList():
     return jsonify(vulnerabilities)
 
 if __name__ == "__main__":
