ef853b6ef940e19bcf37d6656efc0a41a147d5b7
Ethan <mcfarlane-e1@ulster.ac.uk>
2024-03-21 14:17:51 +0000

    Added functionality to list all packages within the database on one page for user browsing. Will be updated to include multiple fields within documents, as well as potentially swapping to Angular 17's @for function rather than ngFor

--- angular-frontend/src/app/vulnerabilities/vulnerabilities.component.ts
+++ angular-frontend/src/app/vulnerabilities/vulnerabilities.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-vulnerabilities',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vulnerabilities.component.html',
   styleUrl: './vulnerabilities.component.css'
 })
 export class VulnerabilitiesComponent {
+  packages: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getPackages();
+  }
+
+  getPackages() {
+    this.http.get<any[]>('http://localhost:5000/vulnerabilities').subscribe(data => {
+      this.packages = data;
+    });
+  }
 }
--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,32 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 from pymongo import MongoClient
 import json
+from flask_cors import CORS
+from bson import ObjectId
 
 app = Flask(__name__)
+CORS(app)
 
 client = MongoClient("localhost", 27017)
 db = client["ProjectS"]
 
 package_collection = db["packages"]
 
-vulnerabilities = [
-    {
-        "package_name": "bootstrap",
-        "current_version": "5.3",
-        "vulnerabilities": [
-            "no vulnerabilities found.",
-            "Positive Health"
-        ]
-    }
-]
-
 @app.route("/")
 def index():
-    return jsonify(vulnerabilities)
+    return jsonify()
 
-@app.route("/vulnerabilities")
-def vulList():
-    return jsonify(vulnerabilities)
+@app.route("/vulnerabilities", methods = ['GET'])
+def get_vulList():
+    packages = list(package_collection.find())
+
+    for package in packages:
+        package['_id'] = str(package['_id'])
+
+    return jsonify(packages)
 
 if __name__ == "__main__":
     app.run(debug=True)
